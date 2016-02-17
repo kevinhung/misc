ec8715bc9f71d68d1863256d3ca31345ff62cd56
kevinhung <kevinhung@synology.com>
2016-02-17 20:28:49 +0800

    Make it not weak in shared library

--- weak-link/example-cpp/static-function/foobar.h
+++ weak-link/example-cpp/static-function/foobar.h
@@ -9,7 +9,11 @@
  * Fake availability.h for ease of demo.
  */
 #define AVAILABLE_INT_REGULAR
+#ifdef BUILD_LIBFOOBAR
+#define AVAILABLE_INT_WEAK          AVAILABLE_INT_REGULAR
+#else
 #define AVAILABLE_INT_WEAK          __attribute__((weak))
+#endif
 #define AVAILABLE_INT_SDK_VER_6_0   AVAILABLE_INT_REGULAR
 #define AVAILABLE_INT_SDK_VER_6_1   AVAILABLE_INT_WEAK
 #define SDK_STARTING_FROM(version)  AVAILABLE_INT_##version
