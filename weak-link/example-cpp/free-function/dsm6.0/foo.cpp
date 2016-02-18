2a5b62278a8108d7b2f8df0cc7d57b67d5474484
kevinhung <kevinhung@synology.com>
2016-02-18 09:30:07 +0800

    Example for weak link in C++ free function

---
+++ weak-link/example-cpp/free-function/dsm6.0/foo.cpp
@@ -0,0 +1,14 @@
+/**
+ * The same as ../dsm6.1/foo.c
+ */
+
+#include <cstdio>
+#include <cstdlib>
+
+
+void foo(int value)
+{
+	fprintf(stdout, "I'm %s in DSM %d.%d\n", __FUNCTION__, MAJOR, MINOR);
+}
+
+// vim:ts=4 sts=4 sw=4
---
+++ weak-link/example-cpp/free-function/dsm6.1/bar.cpp
@@ -0,0 +1,14 @@
+/**
+ * New function introduced in DSM 6.1
+ */
+
+#include <cstdio>
+#include <cstdlib>
+
+
+void bar(int value)
+{
+	fprintf(stdout, "I'm %s in DSM %d.%d\n", __FUNCTION__, MAJOR, MINOR);
+}
+
+// vim:ts=4 sts=4 sw=4
